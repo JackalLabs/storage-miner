78201d6d421575edb748cc0d82739cab4a4e40e9
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2021-12-09 22:49:35 -0500

    Setting up the p2p

---
+++ src/p2p.js
@@ -0,0 +1,105 @@
+const Libp2p = require('libp2p');
+const TCP = require('libp2p-tcp');
+const {NOISE, Noise} = require("@chainsafe/libp2p-noise");
+const MPLEX = require('libp2p-mplex');
+const process = require('process');
+const DHT = require('libp2p-kad-dht');
+const MulticastDNS = require('libp2p-mdns');
+const Bootstrap = require('libp2p-bootstrap');
+const crypto = require('libp2p-crypto');
+const { CID } = require('multiformats/cid')
+
+
+const ma = require('multiaddr');
+const {multiaddr , Multiaddr, protocols, resolvers} = ma;
+
+// const {LevelDatastore} = require('datastore-level');
+
+// const datastore = new LevelDatastore('path/to/store');
+
+let P2P = 
+{
+  name: "JACKAL NODE",
+};
+
+P2P.libmultiaddr = ma;
+P2P.crypto = crypto;
+
+P2P.buildNode = (genesis, peerId) => {
+  return new Promise((resolve, reject) => {
+  
+      
+      Libp2p.create({
+        peerId: peerId,
+        addresses: {
+          // add a listen address (localhost) to accept TCP connections on a random port
+          listen: ['/ip4/127.0.0.1/tcp/' + (genesis ? "50699" : "0")],
+        },
+        modules: {
+          transport: [TCP],
+          connEncryption: [NOISE],
+          streamMuxer: [MPLEX],
+          peerDiscovery: [MulticastDNS],
+          dht: DHT,
+        },
+        config: {
+          dht: {
+            enabled: true,
+          },
+          peerDiscovery: {
+            autoDial: true, // Auto connect to discovered peers (limited by ConnectionManager minConnections)
+            // The `tag` property will be searched when creating the instance of your Peer Discovery service.
+            // The associated object, will be passed to the service when it is instantiated.
+            [Bootstrap.tag]: {
+              enabled: true,
+              list: [ // A list of bootstrap peers to connect to starting up the node
+                "/ip4/127.0.0.1/tcp/50699/p2p/Qma2jQt2d2dbNANmaWjwfe5MGd4B7Git9sdpQU4WBq6jVP",
+                '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+                '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+              ],
+            }
+          }
+        }
+      }).then((res) => {
+        P2P.node = res;
+        resolve();
+      });
+    });
+}
+
+P2P.start = (genesis = false, peerId=null) => {
+  return new Promise((resolve, reject) => {
+
+    P2P.buildNode(genesis = genesis, peerId = peerId).then(() => {
+
+      P2P.node.start().then(() => {
+
+        // print out listening addresses
+        console.log('listening on addresses:')
+        P2P.node.multiaddrs.forEach(addr => {
+          console.log(`${addr.toString()}/p2p/${P2P.node.peerId.toB58String()}`);
+        });
+    
+        const stop = () => {
+          P2P.node.stop().then(() => {
+            console.log('libp2p has stopped')
+            process.exit(0);
+          });
+        }
+    
+        process.on('SIGTERM', stop);
+        process.on('SIGINT', stop);
+
+        resolve();
+      });
+      
+  
+  
+    });
+  });
+  
+
+  
+}
+
+module.exports = P2P;
--- src/server.js
+++ src/server.js
@@ -1,29 +1,96 @@
-const _ = require("underscore");
-require('dotenv').config();
-const env = process.env;
-const express = require("express");
-const app = express();
+const p2p = require("./p2p");
+const args = require('easyarguments').argHandler;
+const PeerId = require('peer-id');
 
-function hello(req, res) {
-    res.send('Hello World!');
-}
+const key = new Uint8Array("current_active_node");
 
-function defineRoutes(app) {
-    app.get('/', hello);
-}
-
-function startApp(app, port) {
-    app.listen(port, () => {
-        console.log(`Listening at http://localhost:${port}`)
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
     });
+  }
+
+function strToUInt(str) {
+    return new TextEncoder().encode(str);
 }
 
-function main() {
+function UIntToStr(arr) {
+    return new TextDecoder().decode(arr);
+}
 
-    defineRoutes(app);
-    startApp(app, env.PORT);
+function overwriteActive(p2p, val) {
+    p2p.node.contentRouting.put(key, val, {minPeers: 0}).then(() => {
+        console.log("success!");
+
+        p2p.node.contentRouting.get(key).then((res) => {
+            console.log(UIntToStr(res.val));
+        }).catch((err) => {
+    
+        });
+    }).catch((err) => {
+
+    });
+
     
 }
 
+function openNode(gen, id, debug=true) {
+    p2p.start(genesis=gen, peerId=id).then(() => {
+        
+        
+    
+        const val = strToUInt(id._idB58String);
+
+        setTimeout(() => {
+            p2p.node.contentRouting.get(key).then((res) => {
+                console.log(res);
+                console.log("success!");
+            }).catch((err) => {
+                console.log(err);
+                console.log("Must be genesis run...");
+                overwriteActive(p2p, val);
+            });
+        }, 10000);
+
+        if(debug) {
+            p2p.node.on('peer:discovery', (peerId) => {
+                console.log(`Found peer ${peerId.toB58String()}`)
+            });
+            p2p.node.connectionManager.on('peer:connect', (connection) => {
+                console.log(`Connected to ${connection.remotePeer.toB58String()}`)
+            });
+            p2p.node.connectionManager.on('peer:disconnect', (connection) => {
+                console.log(`Disconnected from ${connection.remotePeer.toB58String()}`)
+            });
+        }
+
+        
+    });
+}
+
+function start(fields) {
+    let gen = false;
+
+    let peerId = '';
+
+    if('k' in fields){
+        gen = true;
+        PeerId.createFromPrivKey(fields.k).then((id) => {
+            openNode(gen, id);
+
+        });
+    }else{
+        PeerId.create({ bits: 1024, keyType: 'RSA' }).then((id => {
+            openNode(gen, id);
+        }));
+    }
+}
+
+function main() {
+    args.addParameter('k', 'Supplies a private-key to use', ['pkey']);
+
+    args.start(start);
+
+}
 
 main();
