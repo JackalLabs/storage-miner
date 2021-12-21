5822148338863b182c99a2ae0126f7d353654ef4
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2021-12-21 17:57:41 -0500

    filecoin api

--- src/interface.js
+++ src/interface.js
@@ -1,25 +1,72 @@
+require("dotenv").config();
+const axios = require('axios');
 
+const RPC_ENDPOINT = 'http://172.17.0.2:7777/rpc/v0';
 
-function main() {
-    
-    let request = require('request')
-
-    let options = {
-        url: 'http://172.17.0.2:7777',
-        method: 'post',
-        headers: {
-        'content-type': 'application/json'
-        }
-    };
-
-    request(options, (error, response, body) => {
-    if (error) {
-        console.error('Error: ', error)
-    } else {
-        console.log('Response: ', body)
-    }
+function postJSON(method, params = null) {
+    return new Promise((resolve, reject) => {
+        axios({
+            method: 'post',
+            url: RPC_ENDPOINT,
+            data: {
+                jsonrpc: "2.0",
+                method: method,
+                id: 1,
+                params: params
+            }
+        }).then((res) => {
+            resolve(res.data);
+        }).catch((err) => {
+            reject(err);
+        });
     });
+}
+
+
+let Wallet = {};
+
+Wallet.balance = () => {
+    return new Promise((resolve, reject) => {
+        postJSON(method="Filecoin.WalletBalance", params=["t1jdlfl73voaiblrvn2yfivvn5ifucwwv5f26nfza"]).then(resolve).catch(reject);
+    });
+};
+
+
+
+let Client = {};
+
+Client.import = (path) => {
+    return new Promise((resolve, reject) => {
+        postJSON(method="Filecoin.ClientImport", params=[{Path: path, IsCAR: true}]).then(resolve).catch(reject);
+    });
+}
+
+let FileCoin = {client: Client, wallet: Wallet};
+
+FileCoin.version = () => {
+    return new Promise((resolve, reject) => {
+        postJSON(method="Filecoin.Version").then(resolve).catch(reject);
+    });
+};
+
+
+function test() {
+    const fs = require('fs');
+    const path = require('path');
+
+   
+    FileCoin.wallet.balance().then((res) => {
+        console.log(res);
+    });
+    
+    FileCoin.client.import(path.join(__dirname, '..', "testimg.png")).then((res) => {
+        console.log(res);
+    });
+
+    
 
 }
 
-main();
+module.exports = FileCoin;
+
+test();
