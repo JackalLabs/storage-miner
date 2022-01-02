0f9921c7447d660df5ea5fd0310ff05cdd7001c8
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2022-01-01 23:22:51 -0500

    removing the filecoin stuff to do dummy tests

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const filecoin = require("../../lotus_interface/interface");
+// const filecoin = require("../lotus_interface/interface.js");
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -6,7 +6,16 @@ const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
 
-const upload = multer({ dest: path.join(__dirname, 'uploads') });
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, path.join(__dirname, 'uploads'));
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
 
 
 function handleUpload(req, res) {
@@ -15,17 +24,43 @@ function handleUpload(req, res) {
 
     console.log(f.path);
 
-    filecoin.client.import(f.path).then((d) => {
-        return res.send(d);
-    }).catch((d) => {
-        return res.send(d);
-    });
+    console.log(f);
+
+    res.sendStatus(200);
+
+
+
+    // filecoin.client.import(f.path).then((d) => {
+    //     return res.send(d);
+    // }).catch((d) => {
+    //     return res.send(d);
+    // });
 }
 
 function main() {
 
     app.post('/upload', upload.single('upload_file'), (req, res) => {
         return handleUpload(req, res);
+    });
+
+    app.get('/download', (req, res) => {
+        let fname = req.query.file;
+        if(fname) {
+            let readstream = fs.createReadStream(path.join(__dirname, "uploads", fname));
+
+            readstream.on('open', function () {
+                readstream.pipe(res);
+            });
+            
+            readstream.on('error', function(err) {
+                res.end(err);
+            });
+            return 0;
+        }
+
+        res.sendStatus(404);
+
+        
     });
 
     app.get('/', (req, res) => {
