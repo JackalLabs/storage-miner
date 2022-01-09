b460dc977c7c2542afa01a1a9e48927fcc6f16b7
Erin Rivas <erin@atomicbear.com>
2022-01-08 23:27:58 -0600

    merging in Erin work

--- src/main.js
+++ src/main.js
@@ -1,90 +1,138 @@
-const filecoin = require("../../lotus_interface");
+// const filecoin = require("../lotus_interface/interface.js");
 const express = require('express');
-const app = express();
-const port = 3000;
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
+const CORS = require('cors');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-        callback(null, path.join(__dirname, 'uploads'));
-    },
-    filename: function (req, file, callback) {
-        callback(null, file.originalname);
-    }
-});
+// const storage = multer.diskStorage({
+//     destination: function (req, file, callback) {
+//         callback(null, path.join(__dirname, 'uploads'));
+//     },
+//     filename: function (req, file, callback) {
+//         callback(null, file.originalname);
+//     }
+// });
 
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
+const app = express();
+const upload = multer();
+const port = 3000;
+const storage = path.join(__dirname, "uploads");
 
+const refCount = 3;
+const fakeRefs = [
+  {
+    name: '3.jpg',
+    cid: 1
+  },
+  {
+    name: '20211120_151515.txt.jkl',
+    cid: 2
+  },
+  {
+    name: 'test.pdf',
+    cid: 3
+  },
+];
 
+app.use(CORS());
 
-function handleUpload(req, res) {
+function handleUpload(file) {
 
-    let f = req.file;
+  /**
+   * Filecoin Secret Sauce
+   * Starts Here
+   */
 
-    console.log(f.path);
+  const workingPath = `${storage}/${file.name}`;
 
-    console.log(f);
+  fs.unlink(workingPath, (err) => {
+    if (err) console.log('No File Saved');
+  });
 
-    // res.sendStatus(200);
+  fs.writeFile(workingPath, file, (err) => {
+    if (err) throw err;
+  });
 
+  const linked = {
+    name: file.name,
+    cid: ++refCount
+  };
 
+  fakeRefs.push(linked);
 
-    filecoin.client.import(f.path).then((d) => {
-        return res.send(d);
-    }).catch((e) => {
-        return res.send(e);
-    });
+  // filecoin.client.import(file.path).then((d) => {
+  //   return res.send(d);
+  // }).catch((e) => {
+  //   return res.send(e);
+  // });
+
+  /**
+   * Filecoin Secret Sauce
+   * Ends Here
+   */
+
+  return linked.cid;
+}
+
+function handleDownlaod(res, cid) {
+
+  /**
+   * Filecoin Secret Sauce
+   * Starts Here
+   */
+
+  const target = fakeRefs.filter(ref => ref.cid == cid)[0];
+  console.dir(target)
+  /**
+   * Filecoin Secret Sauce
+   * Ends Here
+   */
+
+  res.attachment(target.name);
+  res.setHeader('Access-Control-Expose-Headers', '*'); // Refine later
+  res.sendFile(target.name, { root: storage });
+
 }
 
 function main() {
 
-    app.post('/upload', upload.single('upload_file'), (req, res) => {
-        return handleUpload(req, res);
+  app.post('/upload', upload.single('upload_file'), (req, res) => {
+    console.log('upload hit');
+    const cid = handleUpload(req.file);
+    res.status(200);
+    res.send({ cid });
+  });
+
+  app.get('/download', (req, res) => {
+    console.log('download hit');
+    let cid = req.query.file;
+    if (cid) {
+      handleDownlaod(res, cid);
+    } else {
+      res.sendStatus(404);
+    }
+  });
+
+  app.get('/balance', (req, res) => {
+    filecoin.wallet.balance().then((d) => {
+      return res.send(d);
     });
 
-    app.get('/balance', (req, res) => {
-        filecoin.wallet.balance().then((d) => {
-            return res.send(d);
-        });
-        
-    });
+  });
 
-    app.get('/download', (req, res) => {
-        let fname = req.query.file;
-        if(fname) {
-            res.sendFile(path.join(__dirname, "uploads", fname))
-            return 0;
-        }
+  // Remove/Edit Later
+  app.get('/', (req, res) => {
 
-        res.sendStatus(404);
+    res.send({ fakeRefs })
 
-        
-    });
+  });
 
-    app.get('/', (req, res) => {
-        
-        let readstream = fs.createReadStream(path.join(__dirname, "www", "index.html"));
-
-        readstream.on('open', function () {
-            readstream.pipe(res);
-        });
-        
-        readstream.on('error', function(err) {
-            res.end(err);
-        });
-
-    });
-
-    
-      
-    app.listen(port, () => {
-        console.log(`JACKAL listening at http://localhost:${port}`);
-    });
+  app.listen(port, () => {
+    console.log(`JACKAL listening at http://localhost:${port}`);
+  });
 
 }
-
-
 
 main();
