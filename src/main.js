3b69a16c4e4d8acdd2f4f143a85d7fd2d4255a4f
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2022-01-17 11:07:30 -0500

    ipfs core integration

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-const filecoin = require("../../dingo");
+const filecoin = require("../../lotus_interface");
 const express = require('express');
 const app = express();
 const port = 3000;
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
+const IPFS = require('ipfs-core');
 
 const storage = multer.diskStorage({
     destination: function (req, file, callback) {
@@ -19,59 +20,61 @@ const upload = multer({ storage: storage });
 
 
 
-function handleUpload(req, res) {
+function handleUpload(req, res, ipfs) {
 
     let f = req.file;
 
-    console.log(f.path);
+    console.log(f);
 
     let p = "\\mnt\\c" + f.path.substring(2);
     p= p.replaceAll('\\', '/');
-    console.log(p);
 
-    filecoin.client.import(p).then((d) => {
+    fs.readFile(f.path, (err, data) => {
+        if(err) {
+            return res.status(500).send(err);
+        }
 
-        console.log(d);
-
-
+        ipfs.add(data).then((cid) => {
+    
+            filecoin.client.import(p).then((d) => {
+                filecoin.client.dealPieceCID(d.result.Root['/']).then((s) => {
+                    console.log(s);
+                    let CID = s.result.PieceCID['/'];
+                    console.log(CID);
         
-        filecoin.client.dealPieceCID(d.result.Root['/']).then((s) => {
-            // filecoin.client.calcCommP(p).then((s2) => {
-            //     console.log(s2);
-            // }).catch((e) => {
-            //     console.error(e);
-            // });
-            console.log(s);
-            let CID = s.result.PieceCID['/'];
-            console.log(CID);
-
-            filecoin.client.startDeal(d.result.Root['/'], "t3vooeg3synqqbbfibluumnke2dwqgrg4nfb5es2znh36yx4t7eoxtdy6phzmamq2qyenmfbnpzckth5ibympa", "t01000", s.result.PieceCID['/'], /**filecoin.utils.calculatePaddedSize(*/s.result.PayloadSize/**)*/, filecoin.utils.monthsToBlocks(6)).then((g) => {
-                return res.send(g);
+                    filecoin.client.startDeal(d.result.Root['/'], "t3vooeg3synqqbbfibluumnke2dwqgrg4nfb5es2znh36yx4t7eoxtdy6phzmamq2qyenmfbnpzckth5ibympa", "t01000", s.result.PieceCID['/'], /**filecoin.utils.calculatePaddedSize(*/s.result.PayloadSize/**)*/, filecoin.utils.monthsToBlocks(6)).then((g) => {
+                       
+                        g.cid = cid;
+                        return res.send(g);
+                        
+                    }).catch((e) => {
+                        console.error(e);
+                        return res.status(500).send(e);
+                    });
+        
+                }).catch((e) => {
+        
+                    console.error(e);
+                    return res.status(500).send(e);
+                });
+        
+                    
             }).catch((e) => {
                 console.error(e);
-                return res.send(e);
+                return res.status(500).send(e);
             });
-
-        }).catch((e) => {
-
-            console.error(e);
-            return res.send(e);
+    
+        }).catch((err) => {
+            console.error(err);
+            return res.status(500).send(err);
         });
-
-            
-    }).catch((e) => {
-        console.error(e);
-        return res.send(e);
     });
-
-        
-
 }
 
-function main() {
+function startEndPoints(ipfs) {
 
     app.post('/upload', upload.single('upload_file'), (req, res) => {
-        return handleUpload(req, res);
+        return handleUpload(req, res, ipfs);
     });
 
     app.get('/balance', (req, res) => {
@@ -89,9 +92,34 @@ function main() {
     });
 
     app.get('/download', (req, res) => {
+
         let fname = req.query.file;
         if(fname) {
-            res.sendFile(path.join(__dirname, "uploads", fname))
+            const stream = ipfs.cat(fname);
+                
+            let f = function () {
+
+                return new Promise(async (resolve, reject) => {
+                    let buf = [];
+                    for await (const chunk of stream) {
+                        buf.push(chunk);
+                    }
+                    resolve(buf);
+                });
+                
+                
+            }
+
+            f().then((buffer) => {
+                for(c of buffer) {
+                   res.write(c); 
+                }
+                res.end();
+            });
+            
+            
+
+            // res.sendFile(path.join(__dirname, "uploads", fname))
             return 0;
         }
 
@@ -119,6 +147,17 @@ function main() {
     app.listen(port, () => {
         console.log(`JACKAL listening at http://localhost:${port}`);
     });
+}
+
+function main() {
+
+    const ipfs = IPFS.create().then((ipfs) => {
+        startEndPoints(ipfs);
+    }).catch((error) => {
+        console.error(error);
+    });
+
+    
 
 }
 
