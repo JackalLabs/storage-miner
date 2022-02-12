d1d0f97d0b1b7ea3f2dbd2bc722af0997040fbf8
Erin Rivas <erin@atomicbear.com>
2022-02-12 13:14:26 -0600

    preserving /pack in git

--- docker/bundle.sh
+++ docker/bundle.sh
@@ -1,5 +1,6 @@
 find pack ! -path pack -delete
 npm pack --pack-destination pack
+touch docker/.anchor
 cp docker/.env.template pack/.env.template
 cp docker/Dockerfile pack/Dockerfile
 #cp docker/docker-compose.yaml.traefik ../pack/docker-compose.yaml.traefik
