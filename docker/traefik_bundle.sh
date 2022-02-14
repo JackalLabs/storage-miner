c99bd5ce298c1333232e5a9b17398fc206829731
Erin Rivas <erin@atomicbear.com>
2022-02-14 15:07:05 -0600

    dabbles

--- docker/traefik_bundle.sh
+++ docker/traefik_bundle.sh
@@ -4,4 +4,4 @@ touch docker/.anchor
 cp docker/docker.env.traefik.template pack/.env
 cp docker/node.env.template pack/node.env
 cp docker/Dockerfile pack/Dockerfile
-cp docker/docker-compose.yaml.traefik ../pack/docker-compose.yaml.traefik
+cp docker/docker-compose.yaml.traefik ../pack/docker-compose.yaml
